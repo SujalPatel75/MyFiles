d6504892279bc99b3b4c6b7476a44b8cc4d56ba2
Sujal Patel <76617735+SujalPatel75@users.noreply.github.com>
2023-08-22 10:18:50 +0530

    Update SUJALPATEL_internship_array_sort_script210823.js

--- js/SUJALPATEL_internship_array_sort_script210823.js
+++ js/SUJALPATEL_internship_array_sort_script210823.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const alphabeticalButton = document.getElementById("alphabeticalButton");
 const numericButton = document.getElementById("numericButton");
 const outputDiv = document.getElementById("output");
@@ -35,41 +34,3 @@ function getInputArray() {
 function displayArray(arr) {
   outputDiv.innerHTML = arr.join(", ");
 }
-=======
-const alphabeticalButton = document.getElementById("alphabeticalButton");
-const numericButton = document.getElementById("numericButton");
-const outputDiv = document.getElementById("output");
-const inputArrayField = document.getElementById("inputArray");
-
-alphabeticalButton.addEventListener("click", function () {
-  const inputData = getInputArray();
-  if (inputData) {
-    const sortedAlphabetically = inputData
-      .slice()
-      .sort((a, b) => a.toString().localeCompare(b.toString()));
-    displayArray(sortedAlphabetically);
-  }
-});
-
-numericButton.addEventListener("click", function () {
-  const inputData = getInputArray();
-  if (inputData) {
-    const sortedNumerically = inputData.slice().sort((a, b) => a - b);
-    displayArray(sortedNumerically);
-  }
-});
-
-function getInputArray() {
-  const input = inputArrayField.value.trim();
-  if (input === "") {
-    alert("Please enter numbers separated by commas.");
-    return null;
-  }
-  const dataArray = input.split(",").map((item) => parseFloat(item));
-  return dataArray;
-}
-
-function displayArray(arr) {
-  outputDiv.innerHTML = arr.join(", ");
-}
->>>>>>> 96d1c3f6406c17b1ce160a2ce03c867600e213cf
